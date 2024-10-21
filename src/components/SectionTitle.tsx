c377cecb4907078bce1970e8ffa4e78548f7c536
Mark Chavez <markchavez165@gmail.com>
2024-10-20 20:03:35 -0400

    mobile title change

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function SectionTitle({ title }: Props) {
   return (
-    <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-400 text-3xl md:text-4xl text-center justify-center">
+    <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-400 text-3xl md:text-4xl text-center justify-center ml-[25px] md:ml-0">
       {title}
     </h3>
   );
